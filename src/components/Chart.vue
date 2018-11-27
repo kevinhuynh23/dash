e343f467953e53a18a6074ff992e574b13e74a95
Madeleine Prak <mprak@uw.edu>
2018-11-27 13:42:01 -0800

    Update

---
+++ src/components/Chart.vue
@@ -0,0 +1,50 @@
+<template>
+    <LineChart
+        :labels="['January', 'February', 'March', 'April', 'May']"
+        :datasets="$options.datasets"
+        :options="$options.options"
+    ></LineChart>
+</template>
+
+<script>
+import LineChart from '../components/LineChartTemplate'; 
+import numeral from 'numeral';
+
+const datasets = [
+    {
+        label: 'Facebook',
+        borderColor: 'rgba(0, 122, 255, .5)',
+        backgroundColor: 'rgba(0, 122, 255, .1)',
+        data: [300, 700, 450, 750, 450]
+    },
+    {
+        label: 'Google',
+        borderColor: 'rgba(90, 200, 250, 0.5)',
+        backgroundColor: 'rgba(90, 200, 250, 0.1)',
+        data: [600, 550, 750, 250, 700]
+    }
+];
+
+const options = {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero: true,
+            }
+        }]
+  },
+};
+
+export default {
+    name: 'MyChart',
+    datasets,
+    options,
+    components: {
+        LineChart
+    }
+}
+
+</script>
+
+<style>
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,81 @@
+
+<template>
+<!-- This component renders the table on the bottom of the dashboard. -->
+<div>
+    <h2>{{title}}</h2>
+    <v-data-table 
+        :headers="headers"
+        :items="items"
+        class="elevation-1"
+    >
+        <template slot="items" slot-scope="props">
+            <td>{{props.item.name}}</td>
+            <td>{{props.item.a}} </td>
+            <td>{{props.item.b}}</td>
+            <td>{{props.item.c}}</td>
+        </template>
+    </v-data-table>
+</div>
+</template>
+
+<script>
+import vuetify from 'vuetify'
+
+export default {
+    name: 'Table',
+    
+  computed: {
+      title() {
+          return this.$store.state.title;
+      }
+  },
+    data() {
+        return {
+            // Mock data
+            headers: [
+                {
+                    text: '',
+                    align: 'left',
+                    sortable: false,
+                    value: 'name'
+                },
+                {
+                    text: 'Page Views',
+                    value: 'a'
+                },
+                {
+                    text: 'Sign-Ups',
+                    value: 'b'
+                },
+                {
+                    text: 'Ad-Block',
+                    value: 'c'
+                }
+            ],
+            items: [
+                {
+                    value: false,
+                    name: 'Google Analytics',
+                    a: 354225,
+                    b: 5839,
+                    c: 342
+                },
+                {
+                    value: false,
+                    name: 'Facebook Analytics',
+                    a: 93775,
+                    b: 38349,
+                    c: 4329
+                },
+                {
+                    value: false,
+                    name: 'Bing Analytics',
+                    a: 5939,
+                    b: 5493,
+                    c: 1234
+                }
+            ]
+        }
+    }
+}
+</script>
